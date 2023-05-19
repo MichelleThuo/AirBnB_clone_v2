feb100be28d9a5c625137118ad3e2c26ca73e6fe
Michelle Thuo <92251779+MichelleThuo@users.noreply.github.com>
2023-05-19 17:58:10 +0300

    Task 7: Improve engines

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
+    """Defines a ``State`` class"""
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """gets all ``City`` instances linked to a state"""
+            from models import storage
+            from models.city import City
+
+            return [obj for obj in storage.all(City).values()
+                    if obj.state_id == self.id]
+
+    def __repr__(self):
+        """Returns a string representation of the ``State`` class"""
+        return "<State: name={}>".format(self.name)
